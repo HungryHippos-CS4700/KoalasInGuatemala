bb2a95384d7d32bd4d5e62990e9b36404980962e
Daarrik <darrikwh@gmail.com>
2021-10-14 20:08:13 -0700

    player controller

--- KoalasInGuatemala/Assets/Scripts/PlayerController.cs
+++ KoalasInGuatemala/Assets/Scripts/PlayerController.cs
@@ -19,23 +19,6 @@ public class PlayerController : MovementInTree
     // Update is called once per frame
     void Update()
     {
-        Vector3 rightVector;
-        rightVector = new Vector3(-1.0f, 1.0f, 1.0f);
-         if (transform.localScale== rightVector) {
-            facingRight = true;
-        }
-        else{
-            facingRight=false;
-        }
-        if (Input.GetAxisRaw("Horizontal") == 1 && !facingRight)
-        {
-            Flip();
-        }
-        if (Input.GetAxisRaw("Horizontal") == -1 && facingRight )
-        {
-            Flip();
-        }
-
         // Horizontal Movement
         desiredx = 0;
         if (Input.GetAxisRaw("Horizontal") == 1)
@@ -60,11 +43,5 @@ public class PlayerController : MovementInTree
                 desiredy = -verticalSpeed;
             }
         }
-    }
-    void Flip (){
-        facingRight=!facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
     }
 }
