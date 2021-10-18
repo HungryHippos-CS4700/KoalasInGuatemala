bb7258fe1c846360fce35b9a9c7b149fa29d8eac
matthewkhle <mattmattmattle@gmail.com>
2021-10-17 20:22:48 -0700

    Dust kicks up based on direction of movement

--- KoalasInGuatemala/Assets/Scripts/PlayerController.cs
+++ KoalasInGuatemala/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,15 @@ public class PlayerController : MovementInTree
 
     public float horizontalSpeed;
     public float verticalSpeed;
-    public ParticleSystem leafTrail;
-
+    public bool movingRight;
     public bool moving;
+    public ParticleSystem leafTrail;
+    private ParticleSystem.VelocityOverLifetimeModule leafTrailVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
+        leafTrailVelocity = leafTrail.velocityOverLifetime;
         isHit = false;
     }
 
@@ -25,11 +27,13 @@ public class PlayerController : MovementInTree
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             desiredx = horizontalSpeed;
+            movingRight = true;
             moving = true;
         }
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
             desiredx = -horizontalSpeed;
+            movingRight = false;
             moving = true;
         }
 
@@ -47,9 +51,17 @@ public class PlayerController : MovementInTree
             }
         }
 
-        if (moving && Mathf.Abs(velocity.x) < 0.1f && onBranch) {
-            moving = false;
+        if (moving && Mathf.Abs(velocity.x) < 0.0001f && onBranch)
+        {
+            if (movingRight)
+            {
+                leafTrailVelocity.x = 0.8f;
+            } else {
+                leafTrailVelocity.x = -0.8f;
+            }
             createLeafTrail();
+            moving = false;
+
         }
     }
 
