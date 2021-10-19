1c1bc8a105dbf7e87d54f5cbbfde9cc20071ff84
matthewkhle <mattmattmattle@gmail.com>
2021-10-18 18:24:17 -0700

    Added leaf trail (not complete)
    Co-Authored-By: Darrik Houck <77186596+Daarrik@users.noreply.github.com>

--- KoalasInGuatemala/Assets/Scripts/PlayerController.cs
+++ KoalasInGuatemala/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     private float inputVertical;
     private bool allowVertical;
     private bool inTrunk;
+    private bool onBranch;
 
     // Shooting variables
     public GameObject arm;
@@ -20,7 +21,10 @@ public class PlayerController : MonoBehaviour
     public float fireRate;
     public bool canFire;
     public bool isFiring;
-    public bool isMovingRight;
+
+    // Particle System variables
+    public ParticleSystem leafTrail;
+    private ParticleSystem.VelocityOverLifetimeModule leafTrailVelocity;
 
     // Animation variables
     public Animator animator;
@@ -33,7 +37,9 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         allowVertical = false;
         inTrunk = false;
-        
+        onBranch = false;
+        leafTrailVelocity = leafTrail.velocityOverLifetime;
+
     }
 
     // Fire gun
@@ -52,7 +58,7 @@ public class PlayerController : MonoBehaviour
     // Climbing tree
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.gameObject.name == "Tree")
+        if (collider.gameObject.name == "Tree")
         {
             rb.gravityScale = 0f;
             allowVertical = true;
@@ -61,11 +67,12 @@ public class PlayerController : MonoBehaviour
             inTrunk = true;
             // isFiring = false;
         }
+
     }
 
     private void OnTriggerExit2D(Collider2D collider)
     {
-        if(collider.gameObject.name == "Tree")
+        if (collider.gameObject.name == "Tree")
         {
             rb.gravityScale = 1f;
             allowVertical = false;
@@ -73,6 +80,30 @@ public class PlayerController : MonoBehaviour
             arm.GetComponent<SpriteRenderer>().enabled = true;
             inTrunk = false;
         }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Branch")
+        {
+            Debug.Log("aioserfjio");
+            onBranch = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Branch")
+        {
+            Debug.Log("left thahfdjasdf");
+            onBranch = false;
+        }
+    }
+
+    void createLeafTrail()
+    {
+        leafTrail.Play();
     }
 
     // Update is called once per frame
@@ -83,7 +114,7 @@ public class PlayerController : MonoBehaviour
         lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         firePoint.rotation = Quaternion.Euler(0f, 0f, lookAngle);
 
-        if((lookAngle > 90 || lookAngle < -90) && !inTrunk)
+        if ((lookAngle > 90 || lookAngle < -90) && !inTrunk)
         {
             GetComponent<SpriteRenderer>().flipX = true;
             arm.transform.rotation = Quaternion.Euler(180f, 0f, -lookAngle);
@@ -112,13 +143,13 @@ public class PlayerController : MonoBehaviour
 
         // Vertical movement
         inputVertical = Input.GetAxisRaw("Vertical");
-        if(allowVertical)
+        if (allowVertical)
         {
             rb.gravityScale = 0;
             rb.velocity = new Vector2(rb.velocity.x, inputVertical * speed);
         }
 
-        if(Mathf.Abs(inputHorizontal) > 0 || (Mathf.Abs(inputVertical) > 0f && allowVertical))
+        if (Mathf.Abs(inputHorizontal) > 0 || (Mathf.Abs(inputVertical) > 0f && allowVertical))
         {
             animator.SetBool("IsMoving", true);
         }
@@ -127,5 +158,10 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("IsMoving", false);
         }
 
+        if (Mathf.Abs(inputHorizontal) > 0 && onBranch)
+        {
+            leafTrailVelocity.x = 0.8f * inputHorizontal;
+            createLeafTrail();
+        }
     }
 }
