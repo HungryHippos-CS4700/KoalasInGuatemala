2738e6a4718d8270d9814af8db30f127bda41cfb
matthewkhle <mattmattmattle@gmail.com>
2021-11-04 18:19:17 -0700

    Update Score.cs

--- KoalasInGuatemala/Assets/Scripts/Score.cs
+++ KoalasInGuatemala/Assets/Scripts/Score.cs
@@ -18,7 +18,6 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // scoreText = GetComponent<Text>();
         scoreValue = 0;
     }
 }
